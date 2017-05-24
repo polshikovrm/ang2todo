20cfb10f3d41069f9b9c4c40ba331f24629a4696
Roman Polshikov <roman.polshikov@sigmalion.com.ua>
2017-05-24 14:40:25 +0300

    todo angular2

---
+++ src/app/index.ts
@@ -0,0 +1,32 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {routing, RootComponent} from './routes';
+import {store} from './reducers';
+
+import {AppComponent} from './containers/App';
+import {FooterComponent} from './components/Footer';
+import {HeaderComponent} from './components/Header';
+import {MainSectionComponent} from './components/MainSection';
+import {TodoItemComponent} from './components/TodoItem';
+import {TodoTextInputComponent} from './components/TodoTextInput';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing,
+    store
+  ],
+  declarations: [
+    RootComponent,
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainSectionComponent,
+    TodoItemComponent,
+    TodoTextInputComponent
+  ],
+  bootstrap: [RootComponent]
+})
+export class AppModule {}
